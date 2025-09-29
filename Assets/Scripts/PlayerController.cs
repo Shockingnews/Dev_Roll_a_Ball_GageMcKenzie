0712473270b128c3bd6c251561f63d82d280e333
Shockingnews <gagemckenzie70@gmail.com>
2025-09-29 15:03:51 -0300

    speedboost

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+using Unity.VisualScripting;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,12 +11,17 @@ public class PlayerController : MonoBehaviour
     private int count;
     private float movementX;
     private float movementY;
+    private bool timerActive;
+    private float currentTime;
     public float speed = 0;
+
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        currentTime = 0;
+        
         winTextObject.SetActive(false);
         SetCountText();
         count = 0;
@@ -33,9 +40,23 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
+        if (timerActive)
+        {
+            currentTime = currentTime + Time.deltaTime;
+        }
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
     }
     void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.CompareTag("SpeedBoost"))
+        {
+                timerActive = true;
+            speed = speed * speed;
+            if (currentTime <= 0)
+            {
+                timerActive = false;
+            }
+        }
         
         if (other.gameObject.CompareTag("PickUp"))
         {
@@ -51,6 +72,18 @@ public class PlayerController : MonoBehaviour
         if (count >= 10)
         {
             winTextObject.SetActive(true);
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
         }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            winTextObject.gameObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+        }
+    }
+    
 }
